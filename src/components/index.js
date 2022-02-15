7e4fdefa23f7bbf4cd4c578bab492a55d641788a
Milton Carlos Katoo <mckatoo@gmail.com>
2022-02-15 19:03:00 -0300

    Export all components from them same place

---
+++ src/components/index.js
@@ -0,0 +1,8 @@
+import Header from './header/index'
+import Layout from './layout/index'
+import NoSearch from './no-search/index'
+import Profile from './profile/index'
+import Repositories from './repositories/index'
+import RepositoryItem from './repository-item/index'
+
+export { Header, Layout, NoSearch, Profile, Repositories, RepositoryItem }
