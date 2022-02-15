88bed6a44cb9b7da2cfe434a0deec248c079109c
Milton Carlos Katoo <mckatoo@gmail.com>
2022-02-15 19:17:39 -0300

    Import all components from them same place

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
-import React from "react";
-import Layout from "./components/layout";
-import NoSearch from "./components/no-search";
-import Profile from "./components/profile";
-import Repositories from "./components/repositories";
-import useGithub from "./hooks/github-hooks";
+import React from 'react'
+import { Layout, NoSearch, Profile, Repositories } from './components'
+import useGithub from './hooks/github-hooks'
 
 const App = () => {
-  const { githubState } = useGithub();
+  const { githubState } = useGithub()
   return (
     <Layout>
       {githubState.hasUser ? (
@@ -24,7 +21,7 @@ const App = () => {
         <NoSearch />
       )}
     </Layout>
-  );
-};
+  )
+}
 
-export default App;
+export default App
